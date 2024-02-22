cba91d6a611e8b3ebcb0820a31c018f04e600d10
William Laursen <Tlaursen8@yahoo.com>
2024-02-22 12:06:15 -0500

    First Class Functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (driversArray) => {
+    const firstTwoDrivers = [driversArray[0], driversArray[1]]
+    return firstTwoDrivers
+    return firstTwoDrivers.slice(0, 2)
+    //return driversArray.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (driversArray) => {
+    return driversArray.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (fareMultiplier) => {
+    return (fare) => {
+        return fare * fareMultiplier
+    }
+}
+
+// const fareDoubler = (fare) => {
+// const twoMultiplier = createFareMultiplier(2)
+// console.log(twoMultiplier)
+// return twoMultiplier(fare)
+// }
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, driversFunction) => {
+    console.log(driversFunction)
+    console.log(drivers)
+    return driversFunction(drivers)
+}
